f8c2dfaac88ce1f9aad0adf285c43567b4a38e53
Mohamed Nasser <xma7med@gmail.com>
2024-09-28 16:49:00 +0300

    Part 04 Attachment Service

---
+++ LinkDev.IKEA.BLL/Common/Services/Attachments/AttachmentService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.BLL.Common.Services.Attachments
+{
+    internal class AttachmentService : IAttachmentService
+    {
+
+        private readonly List<string> _allowedExtentions = new /*List<string>*/() { ".png" , ".jpg" , ".jpeg"};
+        private const int _allowedMaxSize = 2_097_152;
+
+
+        public string Upload(IFormFile file, string folderName)
+        {
+            var extention = Path.GetExtension(file.Name); // "ahmed.jpg" ==> .jpg
+
+            if (!_allowedExtentions.Contains(extention))
+                return null;
+
+            if (file.Length > _allowedMaxSize)
+                return null;
+
+
+
+
+            //var folderPath = $"{Directory.GetCurrentDirectory}\\wwwroot\\files, { folderName}" ;
+
+            var folderPath=Path.Combine(Directory.GetCurrentDirectory(),"wwwroot\\files", folderName);
+
+
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);  
+
+            var fileName = $"{Guid.NewGuid()}{extention}"; // Must be Unique
+
+            var filePath = Path.Combine(folderPath, fileName);  // File Location Places 
+
+            // Streaming : Data Per Time 
+
+            using var fileStream = File.Create(filePath);
+            //using var fileStream = File.Create(filePath);
+
+            file.CopyTo(fileStream);
+            return fileName;
+
+
+        }
+        public bool Delete(string filePath)
+        {
+            if (File.Exists(filePath))
+            { 
+                File.Delete(filePath);
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ LinkDev.IKEA.BLL/Common/Services/Attachments/IAttachmentService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.BLL.Common.Services.Attachments
+{
+    internal interface IAttachmentService
+    {
+        string Upload(IFormFile file , string folderName);
+
+        bool Delete(string filePath);
+
+
+    }
+}
