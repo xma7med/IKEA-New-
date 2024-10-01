bf64729b9090f3694d20f52a45cf800ae2bd17de
Mohamed Nasser <xma7med@gmail.com>
2024-10-01 12:54:04 +0300

    Part 04 Account Controller - SignIn

--- LinkDev.IKEA.PL/Controllers/AccountController.cs
+++ LinkDev.IKEA.PL/Controllers/AccountController.cs
@@ -17,16 +17,17 @@ namespace LinkDev.IKEA.PL.Controllers
 		}
 
 
+        #region SignUp
 
         [HttpGet] // Get : /Account/SignUp
         public IActionResult SignUp()
-        { 
-            return View();  
+        {
+            return View();
         }
 
 
         [HttpPost]
-        public async Task<IActionResult> SignUp(SignUpViewModel model )
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
         {
             if (!ModelState.IsValid)
                 return BadRequest();
@@ -34,49 +35,93 @@ namespace LinkDev.IKEA.PL.Controllers
             // Check That not duplicate user 
 
 
-            var user =await _userManager.FindByNameAsync(model.UserName);
+            var user = await _userManager.FindByNameAsync(model.UserName);
 
             if (user is { })
-            { 
+            {
                 ModelState.AddModelError(nameof(SignUpViewModel.UserName), "This User Already Exist !! ");// بعت الكيي كا انا عايزه يبقى في بروب معينه 
                 return View(model);
 
             }
-                user = new ApplicationUser()
-                {
-                    FName = model.FirstName,
-                    LName = model.LastName,
-                    UserName = model.UserName,
-                    Email = model.Email,
-                    IsAgree = model.IsAgree,
-                };
-                var result = await _userManager.CreateAsync(user);
+            user = new ApplicationUser()
+            {
+                FName = model.FirstName,
+                LName = model.LastName,
+                UserName = model.UserName,
+                Email = model.Email,
+                IsAgree = model.IsAgree,
+            };
+            var result = await _userManager.CreateAsync(user);
 
-                if (result.Succeeded)
-                    return RedirectToAction(nameof(SignIn));
+            if (result.Succeeded)
+                return RedirectToAction(nameof(SignIn));
 
-                // If not Succeed هعدي ع الايرورز الممكنه ممكن ايميل ممكن بلا بلا وابعت الكي كا سترنج فاضي 
-                foreach (var error in result.Errors)
-                    ModelState.AddModelError(string.Empty, error.Description);
+            // If not Succeed هعدي ع الايرورز الممكنه ممكن ايميل ممكن بلا بلا وابعت الكي كا سترنج فاضي 
+            foreach (var error in result.Errors)
+                ModelState.AddModelError(string.Empty, error.Description);
 
-                return View(model);
-
-
+            return View(model);
 
 
 
-		}
 
 
-		#region Sign In 
+        }
 
-		public IActionResult SignIn()
+        #endregion
+
+
+        #region Sign In 
+        
+        [HttpGet]
+        public IActionResult SignIn()
         {
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInViewmodel model)
+        {
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+            var user =await  _userManager.FindByEmailAsync(model.Email);
+           
+            
+            if (user is { })
+            {
+				var flag = await _userManager.CheckPasswordAsync(user, model.Password );
+                if (flag)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user , model.Password, model.RememberMe, true);
+					if (result.IsNotAllowed) // 1- Check if he allowed to sign In 
+						ModelState.AddModelError(string.Empty, "Your Account Is Not Confirmed Yet !!");
+
+                    if (result.IsLockedOut)
+                        ModelState.AddModelError(string.Empty, "Your Account is Loocked");
+
+                   /// if (result.RequiresTwoFactor)
+                   /// { 
+                   ///     // Implement
+                   /// }
+                   /// 
+
+                    if (result.Succeeded)
+                        return RedirectToAction(nameof(HomeController.Index) ,"Home" ); 
+				}
+                
+			}
+            ModelState.AddModelError(string.Empty,"Invalid Login Attemps ");
+            return View(model); 
+
+		}
+
+
 
         #endregion
+
+
+
 
     }
 }
---
+++ LinkDev.IKEA.PL/ViewModels/Identity/SignInViewmodel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LinkDev.IKEA.PL.ViewModels.Identity
+{
+	public class SignInViewmodel
+	{
+		public string Email { get; set; } = null!;
+		[DataType(DataType.Password)]
+		public string Password { get; set; } = null!;
+
+        public bool RememberMe { get; set; }
+    }
+}
